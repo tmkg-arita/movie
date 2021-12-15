2f9a61215e097540d1102d1cc418b59f4a3a1219
t_arita <t_arita@b-forme.net>
2021-12-15 13:08:09 +0900

    cssをmergeした

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -7,7 +7,8 @@
 
 
 <!-- コンテンツの中身 -->
-<div class="border-2 shadow-lg w-1/4 p-2">
+<div {{ $attributes-> merge([
+    'class'=>'border-2 shadow-lg w-1/4 p-2']) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
