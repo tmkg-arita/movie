3ab58bd2521321b2c269c2f33a640cbd76a559cd
t_arita <t_arita@b-forme.net>
2022-01-12 16:02:11 +0900

    Admin＆Ownerのseeder作成。 手動でデータを入れた。

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+        [
+             'name' => 'test1',
+             'email'=> 'owner1@owner.com',
+             'password' => Hash::make('adminadmin2'),
+             'created_at' => '2022/01/12 15:15:15',
+        ],
+        [
+            'name' => 'test',
+            'email'=> 'owner2@owner.com',
+            'password' => Hash::make('adminadmin2'),
+            'created_at' => '2022/01/12 15:15:15',
+        ],
+        [
+            'name' => 'test',
+            'email'=> 'owner3@owner.com',
+            'password' => Hash::make('adminadmin2'),
+            'created_at' => '2022/01/12 15:15:15',
+        ],
+    ]);
+    }
+}
