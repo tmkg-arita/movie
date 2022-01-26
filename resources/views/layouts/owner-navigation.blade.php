62e9e81175ecb405cf946820e15b99ef3bd1ce02
t-a <k.t19860909@gmail.com>
2022-01-26 14:45:45 +0900

    ルート(edit)middlewareの設定、abortの設定。

--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -5,9 +5,11 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('owner.dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    <div class="w-12">
+                        <a href="{{ route('owner.dashboard') }}">
+                            <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
@@ -15,6 +17,12 @@
                     <x-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                       店舗情報
+                    </x-nav-link>
+                    {{-- <x-nav-link :href="route('owner.shops.edit')" :active="request()->routeIs('owner.shops.edit')">
+                        店舗情報編集
+                    </x-nav-link> --}}
                 </div>
             </div>
 
@@ -66,6 +74,12 @@
             <x-responsive-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                店舗情報
+            </x-responsive-nav-link>
+            {{-- <x-responsive-nav-link :href="route('owner.shops.edit')" :active="request()->routeIs('owner.shops.edit')">
+                店舗情報編集
+            </x-responsive-nav-link> --}}
         </div>
 
         <!-- Responsive Settings Options -->
