3e2248b24eaa47fb809b3e10d4fd821e70f7f752
t-a <k.t19860909@gmail.com>
2022-01-27 11:38:44 +0900

    ownerの店舗情報にshop.indexの表示（画像等）

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -10,7 +10,33 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @foreach ($shops as $shop )
-                    {{ $shop ->name }}
+                    {{-- クリックしたらedit画面に推移する。editの場合はパラメータも必要なので、第2引数にidを指定している。 --}}
+                    <div class="w-1/2" p-4>
+                        <a href="{{ route('owner.shops.edit',['shop'=>$shop->id ])}}">
+                   
+                        <div class="border rounded-md p-4">
+                            <div class="mb-4">
+                                @if ($shop->is_selling)
+                                 <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                                @else
+                                 <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="text-xl">
+                            {{ $shop ->name }}
+                        </div>
+                        <div>
+                            @if (empty($shop->filename))
+                               <img src="{{ asset('images/noimage.png') }}">
+                            @else
+                               <img src="{{ asset('storage/shops/' . $shop->filename) }}">   
+                            @endif
+                        </div>
+                    </a>    
+                    </div>
+                    
+                </div>
                         
                     @endforeach
                 </div>
