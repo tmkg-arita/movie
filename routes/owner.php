d6bc8b8bd82fafa3b98df7e3c73e12ed7526384a
t-a <k.t19860909@gmail.com>
2022-02-04 10:41:20 +0900

    商品画像の登録(controller,modelの作成)

--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -42,6 +43,11 @@ Route::prefix('shop')->
      Route::post('update/{shop}',[ShopController::class,'update'])
  ->name('shops.update');
  });
+
+ Route::resource('images',ImageController::class)
+->middleware('auth:owners')
+->except(['show']);
+
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
